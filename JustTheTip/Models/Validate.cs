3f031c428d2cb14b3f2e57fb470edd5813a9247c
rrasmusc <35605790+rrasmusc@users.noreply.github.com>
2018-12-30 02:16:12 +0100

    Added validation class & console app for testing.

---
+++ JustTheTip/Models/Validate.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace JustTheTip.Models {
+    public static class Validate {
+
+        private static bool IsValidLength(string input) {
+            if (input.Length >= 2 && input.Length <= 50)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool Email(string input) {
+            var regex = new Regex(@"[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+");
+            if (regex.IsMatch(input) && IsValidLength(input))
+                return true;
+            else
+                return false;
+        }
+    }
+}
