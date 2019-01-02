7bcd53592affbe3645c264766355df8ed72e6294
rrasmusc <35605790+rrasmusc@users.noreply.github.com>
2019-01-02 13:07:38 +0100

    Database changes.

--- JustTheTip/Models/User.cs
+++ JustTheTip/Models/User.cs
@@ -6,19 +6,21 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace JustTheTip.Models {
     public class User
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        [Key]
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public virtual int UserID { get; set; }
-        public virtual string Username { get; set; }
+        [Required, DataType(DataType.EmailAddress)]
+        public virtual string Email { get; set; }
+        [Required, DataType(DataType.Password)]
         public virtual string Password { get; set; }
+        [Required]
         public virtual string FirstName { get; set; }
+        [Required]
         public virtual string LastName { get; set; }
         [Required]
-        [DataType(DataType.EmailAddress)]
-        public virtual string Email { get; set; }
         public virtual string Gender { get; set; }
         public virtual string SexualOrientation { get; set; }
         public virtual DateTime BirthDate { get; set; }
+        [DataType(DataType.ImageUrl)]
         public virtual string ProfilePicUrl { get; set; }
         public virtual string ZodiacSign { get; set; }
         public virtual string Country { get; set; }
@@ -28,6 +30,6 @@ namespace JustTheTip.Models {
 
         public virtual ICollection<User> Friends { get; set; }
         public virtual ICollection<User> FriendRequests { get; set; }
-
+        public virtual ICollection<FriendCategory> FriendCategories { get; set; }
     }
 }
