385414a8fb70fc89e5d32d655b86abdf0d68d534
rrasmusc <35605790+rrasmusc@users.noreply.github.com>
2019-01-04 10:01:16 +0100

    Database rework (wip)

---
+++ JustTheTip/Models/FriendsModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JustTheTip.Models {
+    public class FriendsModel {
+
+        [Key]
+        public virtual string UserId { get; set; }
+        [Key]
+        public virtual string FriendId { get; set; }
+        public virtual string Category { get; set; }
+
+        public class ProfileDbContext : DbContext {
+            public DbSet<FriendsModel> Friends { get; set; }
+
+            public ProfileDbContext() : base("JustTheTip") { }
+        }
+    }
+}
