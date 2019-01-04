1512170a4bebd18778fb7fe3d746dc14f81d289d
rrasmusc <35605790+rrasmusc@users.noreply.github.com>
2019-01-04 14:06:43 +0100

    Actual working database

--- JustTheTip/Models/FriendsModel.cs
+++ JustTheTip/Models/FriendsModel.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
-using System.Linq;
-using System.Web;
 
 namespace JustTheTip.Models {
     public class FriendsModel {
+        [Key, ForeignKey("User"), Column(Order = 0)]
+        public string UserId { get; set; }
+        public virtual UserModel User { get; set; }
 
-        [Key]
-        public virtual string UserId { get; set; }
-        [Key]
+        [Key, ForeignKey("Friend"), Column(Order = 1)]
         public virtual string FriendId { get; set; }
+        public virtual UserModel Friend { get; set; }
+
         public virtual string Category { get; set; }
 
-        public class ProfileDbContext : DbContext {
-            public DbSet<FriendsModel> Friends { get; set; }
+    }
 
-            public ProfileDbContext() : base("JustTheTip") { }
-        }
+    public class FriendsDbContext : DbContext {
+        public DbSet<FriendsModel> Friends { get; set; }
+
+        public FriendsDbContext() : base("JustTheTip") { }
     }
 }
